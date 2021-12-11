e77fbcfe033db5221440dcbcbf7e1d4795f9c258
Mateo Salcedo Rodriguez <msalcedo@domaintools.com>
2021-12-11 12:31:55 -0500

    config heroku

--- ms_login/settings.py
+++ ms_login/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+
 import environ
+import os
+
 
 env = environ.Env()
 environ.Env.read_env()
@@ -163,7 +166,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
